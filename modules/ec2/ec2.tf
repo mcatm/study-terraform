5eec72f72fd4239e3748f09982502783571d5aae
mcatm <tkcs@pelepop.com>
2020-07-22 12:36:09 +0900

    ec2

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "sandbox" {
   count         = 1
   ami           = "ami-785c491f"
   instance_type = "t2.micro"
-  subnet_id = "subnet-003a16599c355cf5b"
+  subnet_id = "subnet-003a16599c355cf5b" # 俺がデフォルトサブネット消しちゃったから指定が必須になってしまった…。試すときは、この記述削除してください
 
   tags = {
     Name = "${var.prefix}-ec2-${count.index + 1}"
